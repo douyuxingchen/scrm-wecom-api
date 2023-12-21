b53e02e15e62dad771cb46de8cfaaf0855753566
fenghaining <fenghaining@douyuxingchen.com>
2023-12-21 17:42:42 +0800

    feature:企微客户、客户群相关接口

---
+++ src/SCrm/wecom/api/corp_tag/delete/param/CorpTagDeleteParam.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Douyuxingchen\ScrmWecomApi\SCrm\wecom\api\corp_tag\delete\param;
+
+// tag_id和group_id不可同时为空。
+// 如果一个标签组下所有的标签均被删除，则标签组会被自动删除。
+
+class CorpTagDeleteParam
+{
+    // 非必传 | 标签的id列表
+    public $tag_id;
+
+    // 非必传 | 标签组的id列表
+    public $group_id;
+
+    // 非必传 | 授权方安装的应用agentid。仅旧的第三方多应用套件需要填此参数
+    public $agentid;
+}
