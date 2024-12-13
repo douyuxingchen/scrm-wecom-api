298099b712937c0f435fd19fabd20f6618faf1d2
fenghaining <fenghaining@douyuxingchen.com>
2024-12-13 14:37:43 +0800

    feat：更换句子域名

--- src/SCrm/juzi/core/enum/Domain.php
+++ src/SCrm/juzi/core/enum/Domain.php
@@ -4,7 +4,7 @@ namespace Douyuxingchen\ScrmWecomApi\SCrm\juzi\core\enum;
 
 class Domain
 {
-    const HUB_JZ_BOT = 'https://hub.juzibot.com';
+    const HUB_JZ_BOT = 'https://aa-hub.ddregion.com';
 
-    const EX_API_BOT_RANGE = 'https://ex-api.botorange.com';
+    const EX_API_BOT_RANGE = 'https://aa-api.ddregion.com';
 }
